ca3d8a7bcbf70a332793360d52886212d8e5e7d4
Temper <temper_tb@mail.ru>
2023-01-16 23:00:31 +0500

    ДЗ 5

--- readme/apps/blog/src/app/blog-post/blog-post.controller.ts
+++ readme/apps/blog/src/app/blog-post/blog-post.controller.ts
@@ -6,36 +6,60 @@ import { CreatePostDto } from './dto/create-post.dto';
 import { UpdateTypeDto } from '../blog-type/dto/update-type.dto';
 import { PostQuery } from './query/post.query';
 
+@ApiTags('posts')
 @Controller('posts')
 export class BlogPostController {
   constructor(
     private readonly blogPostService: BlogPostService
   ) {}
 
+  @ApiResponse({
+    type: PostRdo,
+    status: HttpStatus.OK,
+    description: 'Post found'
+  })
   @Get('/:id')
   async show(@Param('id') id: number) {
     const existPost = await this.blogPostService.getPost(id);
     return fillObject(PostRdo, existPost);
   }
 
+  @ApiResponse({
+    type: PostRdo,
+    status: HttpStatus.OK,
+    description: 'All posts'
+  })
   @Get('/')
   async index(@Query () query: PostQuery) {
     const posts = await this.blogPostService.getPosts(query);
     return fillObject(PostRdo, posts);
   }
 
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: 'The new post has been successfully created.'
+  })
   @Post('/')
   async create(@Body() dto: CreatePostDto) {
     const newPost = await this.blogPostService.createPost(dto);
     return fillObject(PostRdo, newPost);
   }
 
+  @ApiResponse({
+    status: HttpStatus.NO_CONTENT,
+    description: 'Post deleted'
+  })
   @Delete('/:id')
   @HttpCode(HttpStatus.NO_CONTENT)
   async destroy(@Param('id') id: number) {
     this.blogPostService.deletePost(id);
   }
 
+  @ApiResponse({
+    type: PostRdo,
+    status: HttpStatus.OK,
+    description: 'Post updated'
+  })
   @Patch('/:id')
   async update(@Param('id') id: number, @Body() dto: UpdateTypeDto) {
     const updatedPost = await this.blogPostService.updatePost(id, dto);
@@ -43,5 +67,3 @@ export class BlogPostController {
   }
 }
 
-
-//TODO OpenAPI сделать документацию
